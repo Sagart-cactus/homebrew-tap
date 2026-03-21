c97a5f24b24d0b42168dad7d167c1eb806372a09
Sagart-cactus <sagar.y.trivedi@gmail.com>
2026-03-21 22:56:03 +0700

    sprintfoundry 0.7.0

--- Formula/sprintfoundry.rb
+++ Formula/sprintfoundry.rb
@@ -1,8 +1,8 @@
 class Sprintfoundry < Formula
   desc "Multi-agent orchestration for AI-powered software development"
   homepage "https://github.com/Sagart-cactus/SprintFoundry"
-  url "https://registry.npmjs.org/sprintfoundry/-/sprintfoundry-0.6.0.tgz"
-  sha256 "64590bc6503fbad3f44c39f465ffecc6de749fef4c09a62ebaf817d0cf758560"
+  url "https://registry.npmjs.org/sprintfoundry/-/sprintfoundry-0.7.0.tgz"
+  sha256 "0bf0718e60eb35a13acb1c741397aa04ef374a9461b36eee3bc55b4e9a925c53"
   license "Apache-2.0"
 
   depends_on "node"
@@ -13,6 +13,6 @@ class Sprintfoundry < Formula
   end
 
   test do
-    assert_match "0.6.0", shell_output("#{bin}/sprintfoundry --version")
+    assert_match "0.7.0", shell_output("#{bin}/sprintfoundry --version")
   end
 end
